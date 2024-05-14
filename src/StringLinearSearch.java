eebc14e44d68cb981745e1f7b7cd655bcc80368e
Prithivi36 <prithivipappian@gmail.com>
2024-05-14 17:31:27 +0530

    String Linear search

---
+++ src/StringLinearSearch.java
@@ -0,0 +1,32 @@
+public class StringLinearSearch {
+    public static void main(String[] args) {
+
+        String textValue="Prithivi";
+        System.out.println(stringLinearSearch(textValue,'i'));
+        System.out.println(stringLinearSearch2(textValue,'a'));
+    }
+
+    static boolean stringLinearSearch(String from, char target) {
+
+        if (from.length() == 0)
+            return false;
+
+        for(int i=0 ; i<from.length();i++){
+            if(from.charAt(i)==target)
+                return true;
+        }
+        return false;
+    }
+
+    static boolean stringLinearSearch2(String from, char target) {
+
+        if(from.isEmpty())
+            return false;
+
+        for(char i : from.toCharArray()){
+            if(target==i)
+                return true;
+        }
+        return false;
+    }
+}
