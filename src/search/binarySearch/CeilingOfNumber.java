001787cc91829668c3e47c6dcd4d143e3e873967
Prithivi36 <prithivipappian@gmail.com>
2024-05-15 15:04:30 +0530

    ceiling of a Number

---
+++ src/search/binarySearch/CeilingOfNumber.java
@@ -0,0 +1,26 @@
+package search.binarySearch;
+
+public class CeilingOfNumber {
+    public static void main(String[] args) {
+        int[] sortedArr={2,4,5,7,9,11,12,16,45,98,120};
+        int answer=ceilingSearch(sortedArr,1);
+        System.out.println("Index is "+answer);
+    }
+    static int ceilingSearch(int[] arr, int target){
+        int start=0;
+        int end=arr.length-1;
+
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            if(arr[mid]==target){
+                return mid;
+            }
+            if(arr[mid]<target){
+                start=mid+1;
+            } else if (arr[mid]>target) {
+                end=mid-1;
+            }
+        }
+        return start;
+    }
+}
