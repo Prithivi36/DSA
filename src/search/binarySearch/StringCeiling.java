0292c4dfd2e6d5c350a0fa7bfb6d93690874f791
Prithivi36 <prithivipappian@gmail.com>
2024-05-15 16:10:05 +0530

    String Ceiling : Leet Code

--- src/search/binarySearch/StringCeiling.java
+++ src/search/binarySearch/StringCeiling.java
@@ -2,26 +2,29 @@ package search.binarySearch;
 
 public class StringCeiling {
     public static void main(String[] args) {
-
+        char[] arr={'e','e','g','g'};
+        char answer=stringCeiling(arr,'g');
+        System.out.println(answer);
     }
-    static int stringCeiling(int[] arr, int target){
+    static char stringCeiling(char[] arr, char target){
         int start=0;
         int end=arr.length-1;
 
         if(target>arr[arr.length-1])
-            return 0;
+            return arr[0];
 
         while(start<=end){
             int mid=start+(end-start)/2;
-            if(arr[mid]==target){
-                return mid+1;
-            }
-            if(arr[mid]<target){
+            if(arr[mid]<=target){
                 start=mid+1;
             } else if (arr[mid]>target) {
                 end=mid-1;
             }
         }
-        return start;
+        if(start>arr.length-1){
+                    return arr[0];
+                }
+        return arr[start];
     }
+
 }
