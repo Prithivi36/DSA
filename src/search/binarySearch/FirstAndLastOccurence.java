ad27e9c7a5ddf675221ff788ed6609e40d3a68bc
Prithivi36 <prithivipappian@gmail.com>
2024-05-16 15:47:48 +0530

    First and Last Occurence : Leet Code

--- src/search/binarySearch/FirstAndLastOccurence.java
+++ src/search/binarySearch/FirstAndLastOccurence.java
@@ -4,9 +4,10 @@ import java.util.Arrays;
 
 public class FirstAndLastOccurence {
     public static void main(String[] args) {
-        int[] arr ={};
-
-        System.out.println(Arrays.toString(searchRange(arr,7)));
+        int[] arr ={3,7,7,7,8,10,11,13,16,21};
+        System.out.println("My method "+Arrays.toString(searchRange(arr,7)));
+        int[] answer=new int[] {findAnswer(arr,7,true),findAnswer(arr,7,false)};
+        System.out.println("Kunal's Method "+ Arrays.toString(answer));
     }
     static int binarySearch(int[] arr ,int target){
         int start=0;
@@ -62,5 +63,28 @@ public class FirstAndLastOccurence {
         }
 
         return start-1;
+    }
+
+    //Kunal's Answer
+
+    static int findAnswer(int[] arr,int target , boolean isFirst){
+        int start=0;
+        int end=arr.length-1;
+        int ans=-1;
+        while (start<=end){
+            int mid=start+(end-start)/2;
+            if(arr[mid]>target){
+                end=mid-1;
+            } else if (arr[mid]<target) {
+                start=mid+1;
+            }else {
+                ans=mid;
+                if(isFirst)
+                    end=mid-1;
+                else
+                    start=mid+1;
+            }
+        }
+        return ans;
     }
 }
