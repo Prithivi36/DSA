c51cf1cc2de596894e36c09412b8b97f15d87406
Prithivi36 <prithivipappian@gmail.com>
2024-05-18 23:02:21 +0530

    Rotated Array : Count Rotations

--- src/search/binarySearch/CountRotationsInArray.java
+++ src/search/binarySearch/CountRotationsInArray.java
@@ -4,32 +4,47 @@ import java.util.Arrays;
 
 public class CountRotationsInArray {
     public static void main(String[] args) {
-        int[] arr = { 2,2,9,2};
+        int[] arr = {1,2,3};
         int pivot=findPivot(arr);
         int leftCount=pivot+1;
-        System.out.println(findPivot(arr));
+        System.out.println("p :" +pivot);
+        System.out.println("l :"+arr.length);
     }
     static int findPivot(int[] arr){
         int start=0;
         int end=arr.length-1;
+        int steps=0;
         while(arr.length>1&&start<=end){
+            steps++;
             int mid= start+(end-start)/2;
 
             if(mid<end&&arr[mid]>arr[mid+1]){
+                System.out.println("s :"+steps);
                 return mid;
             } else if (mid>start&&arr[mid]<arr[mid-1]) {
+                System.out.println("s :"+steps);
                 return mid-1;
-            }else {
-                if(arr[start]>arr[start+1])
+            }
+            if (arr[start]>arr[mid]){
+                end=mid-1;
+            } else if (arr[start]<arr[mid]) {
+                start=mid+1;
+            } else{
+                if(mid<end&&arr[start]>arr[start+1]) {
+                    System.out.println("s :" + steps);
                     return start;
-                else if (arr[end]<arr[end-1]) {
+                }
+                else if (mid>start&&arr[end]<arr[end-1]) {
+                    System.out.println("s :"+steps);
                     return end-1;
+
                 }
                 start++;
                 end--;
             }
 
         }
+        System.out.println("s :"+steps);
         return -1;
     }
 }
