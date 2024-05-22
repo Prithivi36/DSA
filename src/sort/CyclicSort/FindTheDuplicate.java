783f7b2543f0fe9751f749ce6a6b1a84efb09bc2
Prithivi36 <prithivipappian@gmail.com>
2024-05-22 09:18:53 +0530

    Leet Code: finding Duplicates

--- src/sort/CyclicSort/FindTheDuplicate.java
+++ src/sort/CyclicSort/FindTheDuplicate.java
@@ -5,8 +5,9 @@ import java.util.Arrays;
 public class FindTheDuplicate {
     public static void main(String[] args) {
         int[] array={1,3,1,2,4};
-        int answer=cyclicSort(array);
+        int answer=cyclicSort2(array);
         System.out.println(Arrays.toString(array));
+        System.out.println(answer);
     }
     static int cyclicSort(int[] arr){
         int i=0;
@@ -19,6 +20,21 @@ public class FindTheDuplicate {
         }
 
         return arr[arr.length-1];
+    }
+    static int cyclicSort2(int[] arr){
+        int i=0;
+        while(i<arr.length){
+            if(arr[i]!=i+1){
+                if(arr[i]!=arr[arr[i]-1]){
+                    swap(arr,i,arr[i]-1);
+                }else {
+                    return arr[i];
+                }
+            } else {
+                i++;
+            }
+        }
+        return -1;
     }
     static void swap(int[] arr ,int e1,int e2){
         int temp=arr[e1];
