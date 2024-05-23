bdc09af91bc3ed76188d91ad7b5f3f050a376e0c
Prithivi36 <prithivipappian@gmail.com>
2024-05-23 11:37:38 +0530

    Linked List Implementation

---
+++ src/DataStructure/LinkedList.java
@@ -0,0 +1,41 @@
+package DataStructure;
+
+public class LinkedList {
+    private Node head;
+    private Node tail;
+
+
+    public void insertFirst(int val){
+        Node newNode= new Node(val);
+        newNode.next=head;
+        head=newNode;
+
+        if(tail==null){
+            tail=head;
+        }
+    }
+
+    public void display(){
+        Node temp=head;
+        while(temp!=null){
+            System.out.print(temp.value+" ->");
+            temp=temp.next;
+        }
+        System.out.println("End");
+    }
+
+
+    class Node{
+        private int value;
+        private Node next;
+
+        public Node(int val){
+            this.value=val;
+        }
+        public Node(int val,Node next){
+            this.value=val;
+            this.next=next;
+        }
+
+    }
+}
---
+++ src/DataStructure/Main.java
@@ -0,0 +1,12 @@
+package DataStructure;
+
+public class Main {
+    public static void main(String[] args) {
+        LinkedList ll=new LinkedList();
+        ll.insertFirst(5);
+        ll.insertFirst(10);
+        ll.insertFirst(15);
+        ll.insertFirst(20);
+        ll.display();
+    }
+}
