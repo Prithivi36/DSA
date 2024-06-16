6c8e2bf13e6d1fc10c042dde5aeb08e90fa7553f
Prithivi <prithivipappian@gmail.com>
2024-06-16 21:04:43 +0530

    Leet Code: Happy Number Problem

---
+++ src/DataStructure/SinglyLL/HappyNumber.java
@@ -0,0 +1,34 @@
+package DataStructure.SinglyLL;
+
+public class HappyNumber {
+    public static void main(String[] args) {
+
+
+        System.out.println(isHappyNumber(19));
+
+    }
+    public static boolean isHappyNumber(int number){
+        int fastPointer=number;
+        int slowPointer=number;
+
+        do{
+            slowPointer=findSquareOfDigit(slowPointer);
+            fastPointer=findSquareOfDigit(findSquareOfDigit(fastPointer));
+        }while(slowPointer!=fastPointer);
+
+
+        return slowPointer==1;
+    }
+    public static int findSquareOfDigit(int num){
+        int digit;
+        int answer=0;
+
+        while(num!=0){
+            digit=num%10;
+            answer=(int)(answer+Math.pow(digit,2));
+            num=num/10;
+        }
+
+        return answer;
+    }
+}
