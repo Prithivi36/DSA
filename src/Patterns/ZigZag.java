e7404e2408b8e027a591d923ef573cd1049bd581
Prithivi36 <prithivipappian@gmail.com>
2024-09-03 21:56:48 +0530

    Zoho Interview Zig Zag Pattern

---
+++ src/Patterns/ZigZag.java
@@ -0,0 +1,24 @@
+package Patterns;
+
+import java.util.List;
+import java.util.Scanner;
+
+public class ZigZag {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n= sc.nextInt();
+        for(int i=1;i<=n;i++){
+            int val=i;
+            for (int j = 1; j <=i; j++) {
+                System.out.print(val + " ");
+                if(j%2==1){
+                    val+=(((n-i)*2)+1);
+                }else{
+                    val+=(((i-j)*2));
+                }
+            }
+            System.out.println();
+        }
+    }
+}
