2d8e3ffa003d7f56b554c0c1244b6536e848edc3
Prithivi36 <prithivipappian@gmail.com>
2024-10-02 23:32:31 +0530

    AVL Tree Practice

--- src/DataStructure/Trees/Main.java
+++ src/DataStructure/Trees/Main.java
@@ -4,12 +4,12 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        BinaryTree binaryTree=new BinaryTree();
-        binaryTree.insert(new Scanner(System.in));
-        binaryTree.print();
-
-        BFS bfs = new BFS();
-        System.out.println(bfs.levelOrder(binaryTree.root));
+//        BinaryTree binaryTree=new BinaryTree();
+//        binaryTree.insert(new Scanner(System.in));
+//        binaryTree.print();
+//
+//        BFS bfs = new BFS();
+//        System.out.println(bfs.levelOrder(binaryTree.root));
         //BST
 //        BinarySearchTree bst=new BinarySearchTree();
 //        bst.populate(new int[]{10,3,5,2,19,21,22,6});
@@ -20,9 +20,9 @@ public class Main {
 //        bst.infix();
 //        bst.postfix();
 
-//        AVLTree avlTree=new AVLTree();
-//        avlTree.populate(new int[]{2,3,5,6,10,19,21,22});
-//        avlTree.display();
-//        System.out.println(avlTree.isBalanced());
+        AVLTree avlTree=new AVLTree();
+        avlTree.populate(new int[]{9,8,7,6,5,4,3,2,1});
+        avlTree.display();
+        System.out.println(avlTree.isBalanced());
     }
 }
--- src/Practice/AvlTree.java
+++ src/Practice/AvlTree.java
@@ -29,7 +29,7 @@ public class AvlTree {
         if(root==null){
             root=new Node(val);
         }
-        insert(root,val);
+        root=insert(root,val);
     }
 
     private Node insert(Node node,int val){
@@ -42,11 +42,77 @@ public class AvlTree {
         if(val> node.val){
             node.right=insert(node.right,val);
         }
-        node.height=Math.max(getHeight(node.left),getHeight(node.right));
-//        arrange(node);
-        return node;
+        node.height=Math.max(getHeight(node.left),getHeight(node.right))+1;
+
+        return balance(node);
 
     }
 
-}
+    private Node leftRotate(Node node){
+        Node child=node.right;
+        Node childLeft=child.left;
+        child.left=node;
+        node.right=childLeft;
+        child.height=Math.max(getHeight(child.left),getHeight(child.right))+1;
+        node.height=Math.max(getHeight(node.left),getHeight(node.right))+1;
+        return child;
+    }
+    private Node rightRotate(Node node){
+        Node child=node.left;
+        Node childRight=child.right;
+        child.right=node;
+        node.left=childRight;
+        child.height=Math.max(getHeight(node.left),getHeight(node.right))+1;
+        node.height=Math.max(getHeight(node.left),getHeight(node.right))+1;
+        return child;
+    }
+    private Node balance(Node node){
+        //left-weighted
+        if(getHeight(node.left)-getHeight(node.right)>1){
+            //left-left weighted
+            if(getHeight(node.left.left)-getHeight(node.left.right)>=0){
+                return rightRotate(node);
+            }
+            //left-right weighted
+            node.left=leftRotate(node.left);
+            return rightRotate(node);
+        }
+        //right weighted
+        if(getHeight(node.left)-getHeight(node.right)<-1){
+            //right-right weighted
+            if(getHeight(node.right.left)-getHeight(node.right.right)<=0){
+                return leftRotate(node);
+            }
+            //right-left weighted
+            node.right=rightRotate(node.right);
+            return leftRotate(node);
+        }
 
+        return node;
+    }
+
+    public void display(){
+        display("Root Node :",root);
+    }
+    public void populate(int[] nums){
+        for(int num : nums){
+            insert(num);
+        }
+    }
+    private void display(String info, Node node){
+        if(node==null){
+            return;
+        }
+        System.out.println(info+node.val);
+        display(node.val+"'s left is",node.left);
+        display(node.val+"'s right is",node.right);
+    }
+
+}
+class Main{
+    public static void main(String[] args) {
+        AvlTree avlTree = new AvlTree();
+        avlTree.populate(new int[] {9,8,7,6,5,4,3,2,1});
+        avlTree.display();
+    }
+}
